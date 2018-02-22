f0167f4588b837bd85b928be9e460f23869d2f1e
Suhas <suhas.2ab@gmail.com>
2018-02-22 22:29:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,6 +18,6 @@ button.onclick = function() {
         }
     };
 
-    request.open('GET','http://suhas2ab.imad.hasura-app.io',true);
+    request.open('GET','http://suhas2ab.imad.hasura-app.io/counter',true);
     request.send(null);
 }
